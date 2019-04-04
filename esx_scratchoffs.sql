8d1605634796629f4ab512ea80b24464a7c0ae51
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-04-04 08:17:31 +0200

    Fixed
    USE `essentialmode`;
    
    INSERT INTO `shops` (`store`, `item`, `price`) VALUES
    ('TwentyFourSeven',	'scratchoff',	5);
    
    INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
    ('scratchoff', 'Rubbellos', -1, 0, 1),
    ('scratchoff_used', 'Benutztes Rubbellos', -1, 0, 1);

--- esx_scratchoffs.sql
+++ esx_scratchoffs.sql
@@ -1,5 +1,9 @@
 USE `essentialmode`;
 
-INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES (7, 'TwentyFourSeven', 'scratchoff', 1);
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('scratchoff', 'Scratchoff Ticket', -1, 0, 1);
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('scratchoff_used', 'Used Scratchoff Ticket', -1, 0, 1);
+INSERT INTO `shops` (`store`, `item`, `price`) VALUES
+('TwentyFourSeven',	'scratchoff',	5);
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('scratchoff', 'Rubbellos', -1, 0, 1),
+('scratchoff_used', 'Benutztes Rubbellos', -1, 0, 1);
+
