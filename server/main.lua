b51d5d1001fbfc463cebebce542e56cb7ec5db25
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-04-04 08:28:08 +0200

    Cleaned Up
    beautify

--- server/main.lua
+++ server/main.lua
@@ -3,26 +3,26 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterUsableItem('scratchoff', function(source)
-  local Player = ESX.GetPlayerFromId(source)
-  TriggerClientEvent('scratchoffs:view', source)
-  Player.removeInventoryItem('scratchoff', 1)
+    local Player = ESX.GetPlayerFromId(source)
+    TriggerClientEvent('scratchoffs:view', source)
+    Player.removeInventoryItem('scratchoff', 1)
 end)
 
 RegisterServerEvent('scratchoffs:dispenseReward')
 AddEventHandler('scratchoffs:dispenseReward', function(amount)
-  local xPlayer = ESX.GetPlayerFromId(source)
-  xPlayer.addAccountMoney('bank', amount)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.addAccountMoney('bank', amount)
 
-  if Config.GiveUsedScratchoffAfterUse then
-    xPlayer.addInventoryItem('scratchoff_used', 1)
-  end
+    if Config.GiveUsedScratchoffAfterUse then
+        xPlayer.addInventoryItem('scratchoff_used', 1)
+    end
 end)
 
 RegisterServerEvent('scratchoffs:dispenseUsedScratchoff')
 AddEventHandler('scratchoffs:dispenseUsedScratchoff', function()
-  local xPlayer = ESX.GetPlayerFromId(source)
-  
-  if Config.GiveUsedScratchoffAfterUse then
-    xPlayer.addInventoryItem('scratchoff_used', 1)
-  end
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if Config.GiveUsedScratchoffAfterUse then
+        xPlayer.addInventoryItem('scratchoff_used', 1)
+    end
 end)
