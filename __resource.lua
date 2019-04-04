42be8029c9a113d8e50b04ccb9f334d5b84376ae
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-04-04 08:21:56 +0200

    Updated
    dependencies {
    	'esx_shops'
    }

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Lotto Scratchoffs'
 
-version '1.2.1'
+version '1.2.2'
 
 client_scripts {
   'config.lua',
@@ -12,4 +12,8 @@ client_scripts {
 server_scripts {
   'config.lua',
   'server/main.lua'
+}
+
+dependencies {
+	'esx_shops'
 }
